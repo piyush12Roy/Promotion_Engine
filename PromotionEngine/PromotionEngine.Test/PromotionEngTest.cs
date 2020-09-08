ca3ff9e2bdc109c4392451a565a2d0f449a7c5ec
newsample23 <newsample23@gmail.com>
2020-09-08 23:34:58 +0530

    Adding Test project and adding hosting method and configuring appsetting.json

---
+++ PromotionEngine/PromotionEngine.Test/PromotionEngTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
+
+namespace PromotionEngine.Test
+{
+    public class PromotionEngTest
+    {
+        public PromotionEngTest()
+        {
+            //Adding hosing method
+
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                })
+                .Build();
+        }
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            //configure appsetting.js
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+        }
+    }
+}
