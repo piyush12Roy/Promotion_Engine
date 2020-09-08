e32addea4da65224a9b4d821395d6cc674aa3dfb
newsample23 <newsample23@gmail.com>
2020-09-08 23:28:57 +0530

    Configure hosting process in app also configuring appsetting.js

--- PromotionEngine/PromotionEngine/Program.cs
+++ PromotionEngine/PromotionEngine/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.IO;
 
 namespace PromotionEngine
 {
@@ -6,7 +9,22 @@ namespace PromotionEngine
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Adding hosing method
+
+            var builder = new ConfigurationBuilder();
+            BuildConfig(builder);
+
+            var host = Host.CreateDefaultBuilder()
+                .ConfigureServices((context, services) =>
+                {
+                })
+                .Build();
+        }
+        static void BuildConfig(IConfigurationBuilder builder)
+        {
+            //configure appsetting.js
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                   .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
         }
     }
 }
